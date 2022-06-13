0aa6231cfc34bb6ed45c1a702cb26eb403490cb6
mwshafer <michael.shafer@nau.edu>
2022-06-13 15:42:24 -0700

    updated codegen exe script to use relative path

--- airspyhf_channelize_codegen_script_exe.m
+++ airspyhf_channelize_codegen_script_exe.m
@@ -5,10 +5,16 @@
 % 
 % See also CODER, CODER.CONFIG, CODER.TYPEOF, CODEGEN.
 
+curr_dir = pwd;
+current_folder_name = curr_dir(find(curr_dir=='/',1,'last')+1:end);
+if ~strcmp(current_folder_name,'airspyhf_channelize')
+    error('This function must be run in the airspyhf_channelize root directory. Navigate to <repo location>/airspyhf_channelize and run again.')
+end
+
 %% Create configuration object of class 'coder.CodeConfig'.
 cfg = coder.config('exe','ecoder',false);
-cfg.CustomInclude = '/home/dasl/airspyhf_channelize/CustomMains';
-cfg.CustomSource = '/home/dasl/airspyhf_channelize/CustomMains/main.c';
+cfg.CustomInclude = [curr_dir,'/CustomMains'];
+cfg.CustomSource  = [curr_dir,'/CustomMains/main.c'];
 cfg.GenCodeOnly = true;
 cfg.GenerateReport = true;
 cfg.ReportPotentialDifferences = false;
@@ -20,7 +26,7 @@ ARGS{1}{1} = coder.typeof(0);
 ARGS{1}{2} = coder.typeof(0);
 
 %% Invoke MATLAB Coder.
-cd('/home/dasl/airspyhf_channelize');
+cd([curr_dir,'/codegen/lib/airspyhf_channelize'])
 tic
 codegen -config cfg airspyhf_channelize -args ARGS{1}
 toc
